f380f44e712bafccd138b0ec00f977402bf67d7a
Sandeep-Kumar1 <git config --global user.email sandeep.ttps@gmail.com>
2026-06-04 21:26:21 +0530

    Added score system

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,10 +1,30 @@
 function Result() {
   const answers =
     JSON.parse(localStorage.getItem("answers")) || [];
+    let score = 0;
+
+answers.forEach((answer) => {
+  if (answer.length > 100) {
+    score += 3;
+  } else if (answer.length > 50) {
+    score += 2;
+  } else if (answer.length > 10) {
+    score += 1;
+  }
+});
 
   return (
     <div className="container">
       <h1>Interview Completed</h1>
+      <h2>Score: {score}/9</h2>
+
+<p>
+  {score >= 7
+    ? "Excellent Performance 🚀"
+    : score >= 4
+    ? "Good Job 👍"
+    : "Keep Practicing 💪"}
+</p>
 
       <h2>Your Answers</h2>
 
